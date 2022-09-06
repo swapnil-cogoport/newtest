19b434ce3858184312c5801af740f7bc41044b92
Rudraprasad Das <brayn003@gmail.com>
2022-09-06 19:56:53 +0530

    adding more rollup tooling

--- packages/tokens/rollup.config.js
+++ packages/tokens/rollup.config.js
@@ -1,7 +1,8 @@
+import path from 'path';
+
 import copy from 'rollup-plugin-copy';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-// import del from 'rollup-plugin-delete';
-// import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
+import renameNodeModules from 'rollup-plugin-rename-node-modules';
 import summary from 'rollup-plugin-summary';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
@@ -11,30 +12,32 @@ export default [
 		input  : ['src/index.ts'],
 		output : [
 			{
-				dir             : 'dist/es',
-				format          : 'esm',
-				sourcemap       : true,
-				preserveModules : true,
+				dir                 : 'dist/es',
+				format              : 'esm',
+				sourcemap           : true,
+				preserveModules     : true,
+				preserveModulesRoot : path.join(__dirname, 'src'),
 			},
 			{
-				dir             : 'dist/cjs',
-				format          : 'cjs',
-				exports         : 'named',
-				sourcemap       : true,
-				preserveModules : true,
+				dir                 : 'dist/cjs',
+				format              : 'cjs',
+				exports             : 'named',
+				sourcemap           : true,
+				preserveModules     : true,
+				preserveModulesRoot : path.join(__dirname, 'src'),
 			},
 		],
-		plugins: [
-			peerDepsExternal(),
+		external : ['react'],
+		plugins  : [
+			postcss({ modules: true }),
 			typescript({
 				useTsconfigDeclarationDir : true,
 				tsconfig                  : 'tsconfig.json',
 				tsconfigOverride          : { compilerOptions: { declarationDir: 'dist/types' } },
 			}),
 			terser(),
+			renameNodeModules('_vendors'),
 			copy({ targets: [{ src: './package.json', dest: 'dist' }] }),
-			// bundleSize(),
-			// sizeSnapshot(),
 			summary({
 				showMinifiedSize : true,
 				showGzippedSize  : true,
--- packages/tokens/src/Button/index.tsx
+++ packages/tokens/src/Button/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import styles from './styles.module.css';
+
 function Button() {
 	return (
-		<div>Button</div>
+		<div style={styles.container}>Button</div>
 	);
 }
 
