66ceb6d308ed22fc04417fdbd45d1f08a8ac0508
Rudraprasad Das <brayn003@gmail.com>
2022-09-06 13:13:11 +0530

    adding new analyzer

--- packages/tokens/rollup.config.js
+++ packages/tokens/rollup.config.js
@@ -1,7 +1,9 @@
+import analyze from 'rollup-plugin-analyzer';
 import copy from 'rollup-plugin-copy';
 // import del from 'rollup-plugin-delete';
 // import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 export default [
@@ -9,17 +11,17 @@ export default [
     input  : ['src/index.ts'],
     output : [
       {
-        dir             : 'dist/es',
-        format          : 'esm',
-        sourcemap       : true,
-        preserveModules : true,
+        dir       : 'dist/es',
+        format    : 'esm',
+        sourcemap : true,
+        // preserveModules : true,
       },
       {
-        dir             : 'dist/cjs',
-        format          : 'cjs',
-        exports         : 'named',
-        sourcemap       : true,
-        preserveModules : true,
+        dir       : 'dist/cjs',
+        format    : 'cjs',
+        exports   : 'named',
+        sourcemap : true,
+        // preserveModules : true,
       },
     ],
     plugins: [
@@ -29,6 +31,8 @@ export default [
         tsconfig                  : 'tsconfig.json',
         tsconfigOverride          : { compilerOptions: { declarationDir: 'dist/types' } },
       }),
+      terser(),
+      analyze(),
       copy({ targets: [{ src: './package.json', dest: 'dist' }] }),
     ],
   },
