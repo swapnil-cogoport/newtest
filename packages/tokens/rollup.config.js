9e7ed7dda09986fbe9acc7a8019b0fdd67c7032a
Rudraprasad Das <brayn003@gmail.com>
2022-09-06 14:18:51 +0530

    adding build summary

--- packages/tokens/rollup.config.js
+++ packages/tokens/rollup.config.js
@@ -1,8 +1,8 @@
-import analyze from 'rollup-plugin-analyzer';
 import copy from 'rollup-plugin-copy';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 // import del from 'rollup-plugin-delete';
 // import dts from 'rollup-plugin-dts';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import summary from 'rollup-plugin-summary';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
@@ -11,17 +11,17 @@ export default [
     input  : ['src/index.ts'],
     output : [
       {
-        dir       : 'dist/es',
-        format    : 'esm',
-        sourcemap : true,
-        // preserveModules : true,
+        dir             : 'dist/es',
+        format          : 'esm',
+        sourcemap       : true,
+        preserveModules : true,
       },
       {
-        dir       : 'dist/cjs',
-        format    : 'cjs',
-        exports   : 'named',
-        sourcemap : true,
-        // preserveModules : true,
+        dir             : 'dist/cjs',
+        format          : 'cjs',
+        exports         : 'named',
+        sourcemap       : true,
+        preserveModules : true,
       },
     ],
     plugins: [
@@ -32,8 +32,14 @@ export default [
         tsconfigOverride          : { compilerOptions: { declarationDir: 'dist/types' } },
       }),
       terser(),
-      analyze(),
       copy({ targets: [{ src: './package.json', dest: 'dist' }] }),
+      // bundleSize(),
+      // sizeSnapshot(),
+      summary({
+        showMinifiedSize : true,
+        showGzippedSize  : true,
+        showBrotliSize   : true,
+      }),
     ],
   },
 ];
